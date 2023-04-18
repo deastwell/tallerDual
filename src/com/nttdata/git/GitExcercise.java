9bf5d99b7fb1eea9a73d6e14c24e738d69ff4220
jtellezr <juan.alejandro.tellez.rubio@emeal.nttdata.com>
2023-04-18 09:42:09 +0200

    Corrección - NTT Data - JATR

---
+++ src/com/nttdata/git/GitExcercise.java
@@ -0,0 +1,23 @@
+package com.nttdata.git;
+
+/**
+ * Clase principal
+ * 
+ * @author NTT Data
+ *
+ */
+public class GitExcercise {
+
+	/**
+	 * Método principal
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		// Impresión.
+		System.out.println("Hola mundo!");
+		System.out.println("Corrección NTT Data");
+	}
+
+}
